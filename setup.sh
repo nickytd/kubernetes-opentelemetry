ab987052ee18bfd12d073ee58e50354521d800e8
Niki Dokovski <nickytd@gmail.com>
2022-05-05 12:19:25 +0200

    use operator to provision the gateway collector

--- setup.sh
+++ setup.sh
@@ -5,17 +5,8 @@ set -eo pipefail
 dir=$(dirname $0)
 source $dir/.includes.sh
 
-check_executables
-check_helm_chart "open-telemetry/opentelemetry-collector"
-
-echo "setting up opentelemetry-collector"
-
 kubectl create namespace otel \
   --dry-run=client -o yaml | kubectl apply -f -
-
-helm upgrade otel-collector open-telemetry/opentelemetry-collector \
-  -n otel -f $dir/collector.yaml \
-  --install --timeout 15m
 
 check_helm_chart "open-telemetry/opentelemetry-operator"
 
@@ -23,4 +14,7 @@ echo "setting up opentelemetry-operator"
 
 helm upgrade otel-operator open-telemetry/opentelemetry-operator \
   -n otel -f $dir/operator.yaml \
-  --install --timeout 15m  
+  --install --wait --timeout 15m
+
+kubectl apply -f $dir/collector.yaml
+

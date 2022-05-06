3a1ee1cde4dff922b4482691be52546557482574
Niki Dokovski <nickytd@gmail.com>
2022-05-06 18:20:18 +0200

    example update

--- setup.sh
+++ setup.sh
@@ -12,9 +12,30 @@ check_helm_chart "open-telemetry/opentelemetry-operator"
 
 echo "setting up opentelemetry-operator"
 
+kubectl get secrets -n logging ofd-opensearch-certificates -o json | jq '.data."tls.crt"' | sed 's/"//g' | base64 -D > $dir/ca.crt
+kubectl create secret generic ofd-ca --from-file $dir/ca.crt -n otel \
+  --dry-run=client -o yaml | kubectl apply -f -
+rm -f $dir/ca.crt
+
 helm upgrade otel-operator open-telemetry/opentelemetry-operator \
   -n otel -f $dir/operator.yaml \
   --install --wait --timeout 15m
 
-kubectl apply -f $dir/collector.yaml
+for var in "$@"
+do
+
+  if [[ "$var" = "--with-operator-collector" ]]; then
+    kubectl apply -f $dir/collector.yaml
+  fi
+done
+
+if [ -z "$@" ]; then
+
+    check_helm_chart "open-telemetry/opentelemetry-collector"
+    echo "setting up opentelemetry-collector"
+
+    helm upgrade otel-collector open-telemetry/opentelemetry-collector \
+       -n otel -f $dir/collector-helm.yaml \
+       --install --timeout 15m
+fi
 
